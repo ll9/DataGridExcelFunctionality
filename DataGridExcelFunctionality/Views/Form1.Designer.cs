6ef2167e7a1d9d80fa8dd7840b3763e4246628db
ll9 <llwierer@web.de>
2019-02-02 11:51:41 +0100

    Delete Cell Content

--- DataGridExcelFunctionality/Views/Form1.Designer.cs
+++ DataGridExcelFunctionality/Views/Form1.Designer.cs
@@ -34,11 +34,13 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(45, 41);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(545, 286);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
             // 
             // Form1
             // 
--- DataGridExcelFunctionality/Views/Form1.cs
+++ DataGridExcelFunctionality/Views/Form1.cs
@@ -41,7 +41,129 @@ namespace DataGridExcelFunctionality
 
         private string GetRandomString()
         {
-            return string.Join("", Enumerable.Range(0, 10).Select(i => (char)random.Next(65, 127)));
+            var words = new string[]
+            {
+                "sponge",
+"uptight",
+"report",
+"start",
+"amusing",
+"toys",
+"beef",
+"tense",
+"store",
+"dusty",
+"grease",
+"nonchalant",
+"expert",
+"boat",
+"talk",
+"push",
+"liquid",
+"circle",
+"deceive",
+"behavior",
+"wide",
+"psychotic",
+"tasteful",
+"synonymous",
+"moaning",
+"shock",
+"hose",
+"bells",
+"hot",
+"health",
+"aloof",
+"neat",
+"adjoining",
+"comb",
+"zebra",
+"oafish",
+"early",
+"zesty",
+"rule",
+"brown",
+"fact",
+"heavenly",
+"thoughtless",
+"elated",
+"fireman",
+"guiltless",
+"greedy",
+"appreciate",
+"chemical",
+"magenta",
+"oranges",
+"sour",
+"oatmeal",
+"lyrical",
+"ring",
+"hilarious",
+"friend",
+"minute",
+"picture",
+"pocket",
+"spiders",
+"serious",
+"nappy",
+"noise",
+"standing",
+"nebulous",
+"fragile",
+"bite",
+"drain",
+"seed",
+"gabby",
+"wind",
+"terrible",
+"purple",
+"interest",
+"three",
+"hook",
+"note",
+"book",
+"eye",
+"messy",
+"efficient",
+"protest",
+"thunder",
+"desert",
+"safe",
+"pet",
+"squalid",
+"office",
+"condemned",
+"wail",
+"spiky",
+"bee",
+"goofy",
+"pump",
+"trashy",
+"describe",
+"symptomatic",
+"abnormal",
+"breezy",
+            };
+            return words[random.Next(words.Count() -1)];
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete || e.KeyCode == Keys.Back)
+            {
+                SetSelectedCellsNull();
+            }
+        }
+
+        private void SetSelectedCellsNull()
+        {
+            foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+            {
+                if (!dataGridView1.Rows[cell.RowIndex].Selected)
+                {
+                    cell.Value = DBNull.Value;
+                }
+            }
         }
     }
 }

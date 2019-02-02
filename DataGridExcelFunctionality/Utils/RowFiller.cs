e450c9d56b0b2fb669775b468b67da9903ad57ca
ll9 <llwierer@web.de>
2019-02-02 13:12:18 +0100

    Cut content | refactoring

---
+++ DataGridExcelFunctionality/Utils/RowFiller.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridExcelFunctionality.Utils
+{
+    internal class RowFiller
+    {
+        private Random random = new Random();
+
+        public void addRows(int n, DataTable table)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                var row = table.NewRow();
+                for (int ii = 0; ii < row.Table.Columns.Count; ii++)
+                {
+                    row[ii] = GetRandomString();
+                }
+                table.Rows.Add(row);
+            }
+        }
+
+        private string GetRandomString()
+        {
+            var words = new string[]
+            {
+                "sponge",
+"uptight",
+"report",
+"start",
+"amusing",
+"toys",
+"beef",
+"tense",
+"store",
+"dusty",
+"grease",
+"nonchalant",
+"expert",
+"boat",
+"talk",
+"push",
+"liquid",
+"circle",
+"deceive",
+"behavior",
+"wide",
+"psychotic",
+"tasteful",
+"synonymous",
+"moaning",
+"shock",
+"hose",
+"bells",
+"hot",
+"health",
+"aloof",
+"neat",
+"adjoining",
+"comb",
+"zebra",
+"oafish",
+"early",
+"zesty",
+"rule",
+"brown",
+"fact",
+"heavenly",
+"thoughtless",
+"elated",
+"fireman",
+"guiltless",
+"greedy",
+"appreciate",
+"chemical",
+"magenta",
+"oranges",
+"sour",
+"oatmeal",
+"lyrical",
+"ring",
+"hilarious",
+"friend",
+"minute",
+"picture",
+"pocket",
+"spiders",
+"serious",
+"nappy",
+"noise",
+"standing",
+"nebulous",
+"fragile",
+"bite",
+"drain",
+"seed",
+"gabby",
+"wind",
+"terrible",
+"purple",
+"interest",
+"three",
+"hook",
+"note",
+"book",
+"eye",
+"messy",
+"efficient",
+"protest",
+"thunder",
+"desert",
+"safe",
+"pet",
+"squalid",
+"office",
+"condemned",
+"wail",
+"spiky",
+"bee",
+"goofy",
+"pump",
+"trashy",
+"describe",
+"symptomatic",
+"abnormal",
+"breezy",
+            };
+            return words[random.Next(words.Count() - 1)];
+        }
+    }
+}
--- DataGridExcelFunctionality/Views/Form1.cs
+++ DataGridExcelFunctionality/Views/Form1.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using DataGridExcelFunctionality.Utils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,132 +21,16 @@ namespace DataGridExcelFunctionality
         {
             InitializeComponent();
 
+            var rowFiller = new RowFiller();
+
             table = new DataTable();
             table.Columns.Add("Name");
             table.Columns.Add("Ort");
-            addRows(10);
+            table.Columns.Add("Straße");
+            rowFiller.addRows(10, table);
 
 
             dataGridView1.DataSource = table;
-        }
-
-        private void addRows(int v)
-        {
-            for (int i = 0; i < v; i++)
-            {
-                var row = table.NewRow();
-                row[0] = GetRandomString();
-                row[1] = GetRandomString();
-                table.Rows.Add(row);
-            }
-        }
-
-        private string GetRandomString()
-        {
-            var words = new string[]
-            {
-                "sponge",
-"uptight",
-"report",
-"start",
-"amusing",
-"toys",
-"beef",
-"tense",
-"store",
-"dusty",
-"grease",
-"nonchalant",
-"expert",
-"boat",
-"talk",
-"push",
-"liquid",
-"circle",
-"deceive",
-"behavior",
-"wide",
-"psychotic",
-"tasteful",
-"synonymous",
-"moaning",
-"shock",
-"hose",
-"bells",
-"hot",
-"health",
-"aloof",
-"neat",
-"adjoining",
-"comb",
-"zebra",
-"oafish",
-"early",
-"zesty",
-"rule",
-"brown",
-"fact",
-"heavenly",
-"thoughtless",
-"elated",
-"fireman",
-"guiltless",
-"greedy",
-"appreciate",
-"chemical",
-"magenta",
-"oranges",
-"sour",
-"oatmeal",
-"lyrical",
-"ring",
-"hilarious",
-"friend",
-"minute",
-"picture",
-"pocket",
-"spiders",
-"serious",
-"nappy",
-"noise",
-"standing",
-"nebulous",
-"fragile",
-"bite",
-"drain",
-"seed",
-"gabby",
-"wind",
-"terrible",
-"purple",
-"interest",
-"three",
-"hook",
-"note",
-"book",
-"eye",
-"messy",
-"efficient",
-"protest",
-"thunder",
-"desert",
-"safe",
-"pet",
-"squalid",
-"office",
-"condemned",
-"wail",
-"spiky",
-"bee",
-"goofy",
-"pump",
-"trashy",
-"describe",
-"symptomatic",
-"abnormal",
-"breezy",
-            };
-            return words[random.Next(words.Count() - 1)];
         }
 
         private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
@@ -157,6 +43,16 @@ namespace DataGridExcelFunctionality
             {
                 PasteToDataGridView();
             }
+            else if (e.Control && e.KeyCode == Keys.X)
+            {
+                CutDataGridViewSelectedContent();
+            }
+        }
+
+        private void CutDataGridViewSelectedContent()
+        {
+            Clipboard.SetText(dataGridView1.GetClipboardContent().GetText());
+            SetSelectedCellsNull();
         }
 
         private void PasteToDataGridView()
@@ -207,10 +103,7 @@ namespace DataGridExcelFunctionality
         {
             foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
             {
-                if (!dataGridView1.Rows[cell.RowIndex].Selected)
-                {
-                    cell.Value = DBNull.Value;
-                }
+                cell.Value = DBNull.Value;
             }
         }
     }
